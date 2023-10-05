d47464de56697feeb2808612878ab94aa42b92e2
carson <carsonfrankovich6@gmail.com>
2023-10-05 00:54:13 -0400

    show garments on collection page

--- client/src/components/Collection.js
+++ client/src/components/Collection.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useQuery } from "@apollo/client";
+import gql from "graphql-tag";
+import Listing from "./Listing";
+
+// query all listings (garments that have forsale = true)
+// display them using the listing component
+
+const GET_ALL_GARMENTS = gql`
+  query GetAllGarments {
+    allGarments {
+      id
+      brand
+      title
+      price
+      color
+      size
+      forSale
+    }
+  }
+`;
 
 function Collection() {
+  const [garments, setGarments] = useState([]);
+  const { loading, error, data } = useQuery(GET_ALL_GARMENTS);
+
+  useEffect(() => {
+    if (!loading && data) {
+      setGarments(data.allGarments);
+    }
+  }, [loading, data]);
+
   return (
     <>
-        collection
+      <h1>COLLECTION</h1>
+      {garments
+        .filter((garment) => garment.forSale)
+        .map((garment) => (
+          <Listing title={garment.title} price={garment.price} />
+        ))}
     </>
-  )
+  );
 }
 
-export default Collection
+export default Collection;
