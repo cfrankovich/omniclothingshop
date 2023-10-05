bea5d9a7d6feaea7b2eaa0e364c7c886a6a602b8
aaron <103127735+6latt@users.noreply.github.com>
2023-10-05 02:14:05 -0400

    added 2 da collection #future

--- client/src/components/Collection.js
+++ client/src/components/Collection.js
@@ -1,11 +1,14 @@
 import React from 'react'
+const items = Array(9).fill(null); // For now, it's an array of nulls. Later you can replace it with your items.
 
 function Collection() {
-  return (
-    <>
-        collection
-    </>
-  )
+    return (
+        <div className="collection-grid">
+            {items.map((item, idx) => (
+                <div key={idx} className="item-placeholder"></div>
+            ))}
+        </div>
+    );
 }
 
 export default Collection
