db1758100695b15e3c3bf3a3f04281b49e8a27aa
aaron <103127735+6latt@users.noreply.github.com>
2023-10-05 02:14:42 -0400

    shitty SIDEBAR doesnt work

---
+++ client/src/components/Sidebar.js
@@ -0,0 +1,14 @@
+function Sidebar() {
+    return (
+        <div className="sidebar-filter">
+            <h3>omniclothing</h3>
+            <ul>
+                <li>Pants</li>
+                <li>Jackets</li>
+                <li>Shirts</li>
+                <li>Shorts</li>
+                {/* ... more filters ... */}
+            </ul>
+        </div>
+    );
+}
