cee6ecf28aebeaca19838dea792b838928f457a1
carson <carsonfrankovich6@gmail.com>
2023-10-11 01:29:34 -0400

    deleting old migrations

--- prisma/migrations/20230817141725_/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Session" (
-    "sid" TEXT NOT NULL,
-    "expires" TIMESTAMP(3) NOT NULL,
-    "session" JSONB NOT NULL,
-
-    CONSTRAINT "Session_pkey" PRIMARY KEY ("sid")
-);
